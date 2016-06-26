e82467a78b8d4c7c1fae4c91104f8b3b71c1e867
mva1991 <vinayabhishek@Vinays-MacBook-Pro-2.local>
2016-06-26 18:22:47 -0400

    final changes cosmetic/functional

--- src/com/reservation/TicketService.java
+++ src/com/reservation/TicketService.java
@@ -3,9 +3,30 @@ package com.reservation;
 import java.util.ArrayList;
 
 public interface TicketService {
-
+/**
+ * 
+ * @param venueLevel
+ * @return number of seats available
+ */
 	int numSeatsAvaliable(Integer venueLevel);
+	
+/**
+ * 
+ * @param numSeats
+ * @param minLevel
+ * @param maxLevel
+ * @param customerEmail
+ * @return ArrayList<Seat> that are holded on behalf of the user
+ * @throws Exception
+ */
 	ArrayList<Seat> findAndHoldSeats(int numSeats , Integer minLevel , Integer maxLevel , String customerEmail) throws Exception;
+
+/**
+ * 
+ * @param numSeats
+ * @param customerEmail
+ * @return true implies success and false implies reservation was unsuccessful
+ */
 	boolean reserveSeats(int numSeats, String customerEmail);
 	
 }
